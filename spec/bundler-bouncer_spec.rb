1a9a41bcb336ea181edb25d88a9fedab7192b7b2
Josh Cheek <josh.cheek@gmail.com>
2011-06-19 16:44:23 -0500

    specify desired behaviour

--- spec/bundler-bouncer_spec.rb
+++ spec/bundler-bouncer_spec.rb
@@ -1,3 +1,20 @@
 describe 'bundler-bouncer' do
-  specify { true.should be }
+  
+  context 'from a file invoked with `bundle exec`' do
+    it 'should exit with status 0'
+    it 'should print nothing to stderr'
+  end
+  
+  context 'from a file invoked without `bundle exec`' do
+    context 'with USE_BUNDLER set to no' do
+      it 'should exit with status 0'
+      it 'should print a warning to stderr'
+    end
+    
+    context 'with USE_BUNDLER not set to no' do
+      it 'should exit with status 1'
+      it 'should print an explanation to stderr'
+    end
+  end
+  
 end
