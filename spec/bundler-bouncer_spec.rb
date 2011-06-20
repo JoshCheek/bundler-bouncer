c36fa8fde44265adc047faddb4344682d329437d
Josh Cheek <josh.cheek@gmail.com>
2011-06-20 00:27:21 -0500

    add scenario for USE_BUNDLER not set
    also extract shared examples

--- spec/bundler-bouncer_spec.rb
+++ spec/bundler-bouncer_spec.rb
@@ -17,12 +17,21 @@ describe 'bundler-bouncer' do
       its(:stderr)      { should be_coloured :yellow }
     end
     
-    context 'with USE_BUNDLER set to force' do
-      subject { run_app :bundler => false, :USE_BUNDLER => 'force' }
+    shared_examples_for 'USE_BUNDLER set to something other than "no"' do
       its(:exitstatus)  { should == 1 }
       its(:stdout)      { should == '' }
       its(:stderr)      { should == bouncer_message(:missing_bundler) }
       its(:stderr)      { should be_coloured :red }
+    end
+    
+    context 'with USE_BUNDLER set to force' do
+      subject { run_app :bundler => false, :USE_BUNDLER => 'force' }
+      it_should_behave_like 'USE_BUNDLER set to something other than "no"'
+    end
+    
+    context 'with USE_BUNDLER not set' do
+      subject { run_app :bundler => false }
+      it_should_behave_like 'USE_BUNDLER set to something other than "no"'
     end
   end
   
