a52706d0c12da3512e08f11fe30ccc5aab4d7f3c
Josh Cheek <josh.cheek@gmail.com>
2011-06-20 01:28:35 -0500

    change missing message to be a bit more jovial

--- lib/bundler/bouncer/messages.rb
+++ lib/bundler/bouncer/messages.rb
@@ -1,7 +1,7 @@
 module Bundler
   module Bouncer
     Messages = {
-      :missing_bundler  => "\e[31mTry running this app again with `bundle exec`\e[0m",
+      :missing_bundler  => "\e[31mNo dice, pal. Try again when you learn to `bundle exec`.\e[0m",
       :turned_off       => "\e[33mWarning: Bundler is not running.\e[0m",
     }
   end
