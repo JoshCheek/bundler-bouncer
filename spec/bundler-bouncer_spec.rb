0d9370af486235616bd2319b6372ad76a251b1aa
Josh Cheek <josh.cheek@gmail.com>
2011-06-19 20:47:51 -0500

    works with bundler

--- spec/bundler-bouncer_spec.rb
+++ spec/bundler-bouncer_spec.rb
@@ -1,8 +1,11 @@
+require "#{File.dirname __FILE__}/spec_helper"
 describe 'bundler-bouncer' do
   
   context 'from a file invoked with `bundle exec`' do
-    it 'should exit with status 0'
-    it 'should print nothing to stderr'
+    subject { run_app 'with_bundler', :bundler => true }
+    its(:exitstatus) { should be_zero }
+    its(:stderr)     { should be_empty }
+    its(:stdout)     { should be_empty }
   end
   
   context 'from a file invoked without `bundle exec`' do
