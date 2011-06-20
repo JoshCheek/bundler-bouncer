0e9a21b2c2d8725946da2a1e913dbb259d23eb7c
Josh Cheek <josh.cheek@gmail.com>
2011-06-19 19:23:48 -0500

    move the responsibility of managing command invocation into the InvokedApp class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,18 @@
-require 'open3'
-
-InvokedApp = Struct.new :exitstatus, :stdout, :stderr
+class InvokedApp < Struct.new(:exitstatus, :stdout, :stderr)
+  require 'open3'
+  def self.invoke(command)
+    Open3.popen3 command do |stdin, stdout, stderr, wait_thr|
+      self.new(wait_thr.value.exitstatus, stdout.read, stderr.read)
+    end
+  end
+end
 
 def run_app(dir, options)
   Dir.chdir "#{File.dirname __FILE__}/mock_projects/#{dir}" do
-    
-    # build up the command
     command = ''
     command << 'bundle exec ' if options[:bundler]
     command << "ruby app.rb"
-    
-    # invoke, and capture results
-    invoked_app = InvokedApp.new
-    Open3.popen3 command do |stdin, stdout, stderr, wait_thr|
-      invoked_app.stdout      = stdout.read
-      invoked_app.stderr      = stderr.read
-      invoked_app.exitstatus  = wait_thr.value.exitstatus
-    end
-    invoked_app
+    InvokedApp.invoke command
   end
 end
 
